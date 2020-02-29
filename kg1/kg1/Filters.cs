c41f57a3177fcab875568f3545dc8687c42e0a69
L0G1NN <329898@mail.ru>
2020-02-29 12:30:57 +0300

    Monkey Code

---
+++ kg1/kg1/Filters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace kg1
+{
+    abstract class Filters
+    {
+        public Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
+        {
+            Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+            for (int x = 0; x < sourceImage.Width; x++)
+            {
+                worker.ReportProgress((int)((float)x / resultImage.Width * 100));
+                for (int y = 0; y < sourceImage.Height; y++)
+                {
+                    resultImage.SetPixel(x, y, calculateNewPixelColor(sourceImage, x, y));
+                }
+            }
+
+            return resultImage;
+        }
+
+        protected abstract Color calculateNewPixelColor(Bitmap sourceImage, int x, int y);
+
+        public int clamp(int value, int min, int max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+    }
+}
