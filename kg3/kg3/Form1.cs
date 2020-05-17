20b7664a4bba6d956491e27f03de7ffa63196aa9
YoungMayne <ostrovsky1699@gmail.com>
2020-05-17 21:50:18 +0300

    Lab 3 done

---
+++ kg3/kg3/Form1.cs
@@ -0,0 +1,141 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kg3
+{
+    public partial class Form1 : Form
+    {
+        private GLControl panel;
+        private Shader shader;
+        private Thread updateThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.panel = new OpenTK.GLControl(new OpenTK.Graphics.GraphicsMode(ColorFormat.Empty, 24, 0, 8));
+            panel.Paint += GLPaint;
+
+            this.SuspendLayout();
+
+            this.Width  = 800;
+            this.Height = 800;
+
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(this.Width, this.Height);
+            this.panel.TabIndex = 0;
+
+            this.Controls.Add(this.panel);
+            this.ResumeLayout(false);
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            panel.MakeCurrent();
+
+            shader = new Shader(panel.Width, panel.Height);
+
+            updateThread = new Thread(Update);
+            updateThread.Start();
+        }
+
+
+        private void GLPaint(object sender, PaintEventArgs e)
+        {
+            panel.MakeCurrent();
+            shader.DrawQuads();
+            panel.SwapBuffers();
+        }
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.panel.Width  = this.Width;
+            this.panel.Height = this.Height;
+
+            if (this.Created)
+                shader.Resize(panel.Width, panel.Height);
+
+            this.Closed += CloseEvent;
+        }
+
+
+        private new void Update()
+        {
+            bool directionX = true;
+            bool directionZ = true;
+            float step = 0.00001f;
+
+            while(true)
+            {
+                switch(directionX)
+                {
+                    case true:
+                        while (shader.lightPostion.X < 4)
+                        {
+                            shader.lightPostion.X += step;
+                            panel.Invalidate();
+                        }    
+                        break;
+
+                    case false:
+                        while (shader.lightPostion.X > -4)
+                        {
+                            shader.lightPostion.X -= step;
+                            panel.Invalidate();
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                switch (directionZ)
+                {
+                    case true:
+                        while (shader.lightPostion.Z < 3)
+                        {
+                            shader.lightPostion.Z += step;
+                            panel.Invalidate();
+                        }
+                        break;
+
+                    case false:
+                        while (shader.lightPostion.Z > -3)
+                        {
+                            shader.lightPostion.Z -= step;
+                            panel.Invalidate();
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                directionX = !directionX;
+                directionZ = !directionZ;
+            }
+        }
+
+
+        public void CloseEvent(object sender, System.EventArgs e)
+        {
+            updateThread.Abort();
+        }
+
+    }
+}
---
+++ kg3/kg3/View.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Numerics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+
+class Shader
+{
+    private int width;
+    private int height;
+    private int id;
+    public OpenTK.Vector3 lightPostion;
+
+
+    public Shader(int width, int height)
+    {
+        this.lightPostion = new OpenTK.Vector3(2.0f, 4.0f, -4.0f);
+
+        GL.ShadeModel(ShadingModel.Smooth);
+        GL.MatrixMode(MatrixMode.Projection);
+        GL.LoadIdentity();
+
+        Resize(width, height);
+
+        id = GL.CreateProgram();
+
+        InitShader(width, height);
+    }
+
+
+    private void InitShader(int width, int height)
+    {
+        string repositoryPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), @"..\..\"));
+
+        LoadShader(repositoryPath + "raytracing.vert", id, out int vertexShader);
+        LoadShader(repositoryPath + "raytracing.frag", id, out int fragmentShader);
+
+        GL.LinkProgram(id);
+
+        GL.GetProgram(id, GetProgramParameterName.LinkStatus, out int status);
+        Console.WriteLine(GL.GetProgramInfoLog(id));
+    }
+
+
+    public void Resize(int width, int height)
+    {
+        this.width  = width;
+        this.height = height;
+
+        GL.Ortho(0, width, 0, height, -1, 1);
+        GL.Viewport(0, 0, width, height);
+    }
+
+
+    public void LoadShader(string filename, int program, out int address)
+    {
+        switch (Path.GetExtension(filename))
+        {
+            case ".vert":
+                address = GL.CreateShader(ShaderType.VertexShader);
+                break;
+
+            case ".frag":
+                address = GL.CreateShader(ShaderType.FragmentShader);
+                break;
+
+            default:
+                address = 0;
+                break;
+        }
+
+        StreamReader reader = new StreamReader(filename);
+
+        GL.ShaderSource(address, reader.ReadToEnd());
+
+        GL.CompileShader(address);
+        GL.AttachShader(program, address);
+
+        Console.WriteLine(GL.GetShaderInfoLog(address));
+
+        reader.Close();
+    }
+
+
+    public void DrawQuads()
+    {
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+        GL.UseProgram(id);
+
+        GL.Uniform3(GL.GetUniformLocation(id, "LIGHT_POSITION"), lightPostion);
+
+        GL.Begin(PrimitiveType.Quads);
+
+        GL.Vertex2(-width, -height);
+        GL.Vertex2(width, -height);
+        GL.Vertex2(width, height);
+        GL.Vertex2(-width, height);
+
+        GL.End();
+    }
+}
